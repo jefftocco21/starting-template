391f2d309d4b0e1ff1d24875fdbb25378e8b23d3
Jeff Tocco <jnt369@gmail.com>
2026-04-28 13:47:43 -0400

    extracted feed item

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,49 @@ Route::get('/', function () {
 });
 
 Route::get('/feed', function () {
-    return view('feed');
+    $feedItems = json_decode(json_encode([
+        [   
+            'postedDateTime' => '3h',
+            'content' => <<<str
+            <p>
+                I made this! <a href="#">#myartwork</a> <a href="#">#pixl</a>
+            </p>
+            <img src="/images/simon-chilling.png" alt="" />
+            str,
+            'likeCount' => 45,
+            'replyCount' => 5,
+            'repostCount' => 151,
+            'profile' => [
+                'avatar' => '/images/michael.png',
+                'displayName' => 'Michael',
+                'handle' => '@mmich_jj',
+            ]
+        ]
+    ]));
+
+    return view('feed', compact('feedItems'));
 });
 
 Route::get('/profile', function () {
-    return view('profile');
+    $feedItems = json_decode(json_encode([
+        [   
+            'postedDateTime' => '3h',
+            'content' => <<<str
+            <p>
+                I made this! <a href="#">#myartwork</a> <a href="#">#pixl</a>
+            </p>
+            <img src="/images/simon-chilling.png" alt="" />
+            str,
+            'likeCount' => 45,
+            'replyCount' => 5,
+            'repostCount' => 151,
+            'profile' => [
+                'avatar' => '/images/michael.png',
+                'displayName' => 'Michael',
+                'handle' => '@mmich_jj',
+            ]
+        ]
+    ]));
+
+    return view('profile', compact('feedItems'));
 });
